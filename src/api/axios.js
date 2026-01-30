42236944106613d7557f2eaa97c899e81e1bb220
Mukesh Kumar <mk3304413@gmail.com>
2026-01-30 16:43:47 +0530

    Fix issue in backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://eventify-backend-ne49.onrender.com',
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Add a request interceptor to include the token in all requests
